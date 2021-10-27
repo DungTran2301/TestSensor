e4c8ca252dd911fd9adba368039f4da017e36501
Dang Dung <dangdung239>
2021-10-27 10:12:48 +0700

    sensor is running

--- app/src/main/java/com/dungtran/demostepcounter/MainActivity.kt
+++ app/src/main/java/com/dungtran/demostepcounter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dungtran.demostepcounter
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -10,82 +11,70 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.dungtran.demostepcounter.databinding.ActivityMainBinding
+import java.util.jar.Manifest
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var binding: ActivityMainBinding
 
-    private var sensorManager: SensorManager? = null
+    private lateinit var sensorManager: SensorManager
+
+    private lateinit var sensor: Sensor
     private var running = false
-    private var totalStep = 0f
-    private var previousTotalSteps = 0f
+    private var magnitudePrevious = 0.0
+    private var stepCount = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        loadData()
-        resetStep()
+
+        Log.d("Main", "onCreate: test onCreate work")
+
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
     }
 
+    override fun onSensorChanged(event: SensorEvent) {
+        Toast.makeText(this, "In sensor change", Toast.LENGTH_LONG).show()
+
+        Log.d("Main", "onSensorChanged: sensor is changing")
+        val x = event.values[0]
+        val y = event.values[1]
+        val z = event.values[2]
+
+        val magnitude = sqrt(x*x + y*y +z*z)
+        val magnitudeDelta = magnitude - magnitudePrevious
+        magnitudePrevious = magnitude.toDouble()
+
+        if (magnitudeDelta > 6) stepCount++
+        binding.tvStepCounter.text = (""+stepCount)
+    }
     override fun onResume() {
         super.onResume()
-        running = true
-        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-        if (stepSensor != null) {
-            sensorManager!!.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL)
-        } else {
-            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
-        }
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
-        running = false
-        sensorManager?.unregisterListener(this)
+        sensorManager.unregisterListener(this)
+
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        Log.d("main", "onSensorChanged: $totalStep")
-        if (running) {
-//            totalStep = event!!.values[0]
-//            val currentStep = totalStep.toInt() - previousTotalSteps.toInt()
-            binding.tvStepCounter.text = ("" + event!!.values[0])//("$currentStep")
-        }
-        Toast.makeText(this, "Sensor change", Toast.LENGTH_SHORT).show()
-    }
+
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    private fun resetStep() {
-        binding.btnReset.setOnClickListener {
-            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_LONG).show()
-            Log.d("main", "resetStep: tap")
-        }
-        binding.btnReset.setOnLongClickListener {
-            previousTotalSteps = totalStep
-            Log.d("main", "resetStep: $totalStep")
-            binding.tvStepCounter.text = "120"
-            saveData()
-            true
-        }
-    }
 
-    private  fun saveData() {
-        val sharedPreferences = getSharedPreferences("myPre", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putFloat("primaryKey", previousTotalSteps)
-        editor.apply()
-    }
-
-    private fun loadData() {
-        val sharedPreferences = getSharedPreferences("myPre", Context.MODE_PRIVATE)
-        val savedNumber = sharedPreferences.getFloat("primaryKey", 0f)
-        previousTotalSteps = savedNumber
-
-        Log.d("Main", "loadData: 000000000000")
-    }
 }
